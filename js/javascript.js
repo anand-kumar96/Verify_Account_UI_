d26b87b26578a441162f4ea3edaea527ffdb44ff
Anand <hyeiamaka06@gmail.com>
2022-12-01 22:28:14 +0530

    code

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,15 @@
 const codes = document.querySelectorAll('.code');
 codes[0].focus();
+
 codes.forEach((code, idx) => {
 code.addEventListener('keydown', (e) => {
 if(e.key >= 0 && e.key <= 9) {
     codes[idx].value = '';
+  if(idx==5){
+    return;
+  }
    setTimeout(() => codes[idx +1].focus(),10)
-}else if(e.key ==='Backspace'){
+}else if(e.key ==='Backspace' && idx > 0){
     setTimeout(() => codes[idx-1].focus(), 10)
 }
 })
